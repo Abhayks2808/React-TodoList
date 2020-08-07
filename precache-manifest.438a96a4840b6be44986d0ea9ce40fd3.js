965c41615a5855b379477674b47ddf41411b5752
Abhayks2808 <Abhayks2808@gmail.com>
2020-08-07 11:56:13 +0530

    Updates

---
+++ precache-manifest.438a96a4840b6be44986d0ea9ce40fd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59637214eaceeeaf94e8038bb1629093",
+    "url": "/Abhayks2808/React-TodoList.git/index.html"
+  },
+  {
+    "revision": "5331861be1869bcd163d",
+    "url": "/Abhayks2808/React-TodoList.git/static/css/main.486367e2.chunk.css"
+  },
+  {
+    "revision": "3db7f49351acb79c6187",
+    "url": "/Abhayks2808/React-TodoList.git/static/js/2.df7c2a71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Abhayks2808/React-TodoList.git/static/js/2.df7c2a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5331861be1869bcd163d",
+    "url": "/Abhayks2808/React-TodoList.git/static/js/main.6f9557d5.chunk.js"
+  },
+  {
+    "revision": "5510c2130a5976e5c22a",
+    "url": "/Abhayks2808/React-TodoList.git/static/js/runtime-main.71a8f4b7.js"
+  }
+]);
